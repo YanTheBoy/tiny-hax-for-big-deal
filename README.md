4513690eff758d15b84547339e9d1c1252d0cfa3
Ilya Arkhipov <iliarkhpv@gmail.com>
2020-04-24 19:36:17 +0300

    Uploading 1st version of script and README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# tiny-hax-for-big-deal
-Script helps with e-diary hacking
+# E-diary cracker
+Script change bad marks (2,3) on good marks, delete all remarks and create praises.
+# How to install
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+Download script in directory near **manage.py** and open *shell*
+```bash
+$ python3 manage.py shell
+Python 3.6.9 (default, Nov  7 2019, 10:44:02) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+(InteractiveConsole)
+>>> 
+```
+If you want raise all your marks, import **fix_marks** function and use it with your name:
+```bash
+>>> from script import fix_marks
+>>> fix_marks('Дуров Павел')
+```
+If you need to delete bad comments, use **delete_chastisement** function:
+```bash
+>>> from script import delete_chastisement
+>>> delete_chastisement('Дуров Павел')
+```
+For creation good uniq comments for last subject about you , import **create_commendation** and additionaly 
+indicate subject name:
+```bash
+>>> from script import create_commendation
+>>> create_commendation('Дуров Павел', 'Литература')
+```
+# Project Goals
+The code is written for educational purposes on online-course for web-developers [DEVMAN.org](https://devman.org)
---
+++ script.py
@@ -0,0 +1,40 @@
+from datacenter.models import Mark, Chastisement, Lesson, Commendation, Schoolkid
+from random import choice
+
+
+def fix_marks(schoolkid_name_surname):
+    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name_surname)
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
+    for mark in marks:
+        mark.points = choice([4, 5])
+        mark.save()
+
+
+def delete_chastisement(schoolkid_name_surname):
+    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name_surname)
+    chantisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    for chantisement in chantisements:
+        chantisement.delete()
+
+
+def create_commendation(schoolkid_name_surname, subject_title):
+    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name_surname)
+    last_lesson = Lesson.objects.filter(
+        group_letter=schoolkid.group_letter,
+        year_of_study=schoolkid.year_of_study,
+        subject__title=subject_title).order_by('-date')[0]
+    praises = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+               'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+               'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
+               'Ты сегодня прыгнул выше головы!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!',
+               'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!',
+               'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
+               'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!', 'Я поражен!'
+               ]
+    Commendation.objects.create(
+        text=choice(praises),
+        created=last_lesson.date,
+        schoolkid=schoolkid,
+        subject=last_lesson.subject,
+        teacher=last_lesson.teacher
+    )
